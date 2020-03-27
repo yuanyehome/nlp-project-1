b1e17a0c18b1f5c9634bc8f24adcacaa5a852681
yuanyehome <2252706531@qq.com>
2020-03-27 15:20:33 +0800

    acce

--- src/KNN.py
+++ src/KNN.py
@@ -2,43 +2,69 @@ import numpy as np
 import pickle
 from tqdm import tqdm
 import time
+import math
 
-word2idx = {}
-idx = 0
 data = []
+data_arr = []
+acc = []
+res_f = open("../res/res-" + time.strftime("%Y-%m-%d-%H:%M:%S",
+                                           time.localtime()) + ".txt", "w")
 K = 20
+# Variable `data` contains all the data item with format of (label, text embedding, |embedding|^2). The embedding is a vector of |V| dimension, where |V| is the size of vacabulary. The i-th element of this vector is 1 of this text contains word_i, else 0.
+# Divide the data into 10 parts, we can get `data_arr`
+# Variable `acc` records the accuracy of each test.
+# Variable `res_f` is the file which the result will be written to.
+# `K` is the hyperparameter.
 
 
 def calc_cos(arr1, arr2):
-    assert (len(arr1) == len(arr2))
-    return np.inner(arr1, arr2) / np.sqrt(np.inner(arr1, arr1) * np.inner(arr2, arr2))
+    '''
+    Calculate the cos similarity
+    '''
+    # assert (len(arr1) == len(arr2))
+    return np.inner(arr1[1], arr2[1]) / math.sqrt(arr1[2] * arr2[2])
 
 
-with open("../data/all_data.pkl", "rb") as f:
-    all_data = pickle.load(f)
+def build_data():
+    '''
+    Build the dataset and divide it into 10 parts.
+    '''
+    word2idx = {}
+    idx = 0
+    global data
+    global data_arr
+    with open("../data/all_data.pkl", "rb") as f:
+        all_data = pickle.load(f)
 
-for item in all_data:
-    for word in item[1]:
-        if word not in word2idx.keys():
-            word2idx[word] = idx
-            idx += 1
+    # build a index, which map a word into a index of embedding vector.
+    for item in all_data:
+        for word in item[1]:
+            if word not in word2idx.keys():
+                word2idx[word] = idx
+                idx += 1
 
-for item in all_data:
-    data_item = (item[0], np.zeros(len(word2idx)))
-    for word in item[1]:
-        data_item[1][word2idx[word]] = 1
-    data.append(data_item)
+    # build vectors.
+    for item in all_data:
+        data_item = [item[0], np.zeros(len(word2idx)), 0]
+        for word in item[1]:
+            data_item[1][word2idx[word]] = 1
+            data_item[2] += 1
+        data.append(data_item)
 
-np.random.shuffle(data)
-data_arr = []
-length = len(data) // 10
-for i in range(9):
-    data_arr.append(data[i * length:(i + 1) * length])
-data_arr.append(data[9 * length:])
+    # divide the data into 10 parts
+    np.random.shuffle(data)
+    length = len(data) // 10
+    for i in range(9):
+        data_arr.append(data[i * length:(i + 1) * length])
+    data_arr.append(data[9 * length:])
 
-acc = []
-res_f = open("../res/res-" + time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()) + ".txt", "w")
-for i in range(10):
+
+def run_test(i):
+    '''
+    Use data_arr[i] as test set and the left data as train set.
+    '''
+
+    # get test data and train data
     test_data = data_arr[i]
     train_data = []
     all_num = len(test_data)
@@ -46,14 +72,23 @@ for i in range(10):
     for j in range(10):
         if j != i:
             train_data += data_arr[j]
+
+    # for each item in test set, find the k-nearest-neighbor.
     for item in tqdm(test_data):
         labels = {}
-        record = list(map(lambda train_item: (train_item[0], calc_cos(item[1], train_item[1])),
+
+        # for each item in train set, calculate cos similarity of test item and train item
+        # record contains item formatted as (label, similarity).
+        record = list(map(lambda train_item: (train_item[0], calc_cos(item, train_item)),
                           train_data))
+
+        # find the max-k labels
         record.sort(key=lambda s: s[1], reverse=True)
         for m in range(K):
             labels.setdefault(record[m][0], 0)
             labels[record[m][0]] += 1
+
+        # find the most frequent label
         res = ''
         Max = 0
         for neighbor in labels.keys():
@@ -65,5 +100,11 @@ for i in range(10):
     acc.append(acc_num / all_num)
     print("accuracy in test%d : %f" % (i, acc_num / all_num))
     print("accuracy in test%d : %f" % (i, acc_num / all_num), file=res_f)
-print("average accuracy: %f" % (np.mean(acc)))
-print("average accuracy: %f" % (np.mean(acc)), file=res_f)
+
+
+if __name__ == "__main__":
+    build_data()
+    for i in range(10):
+        run_test(i)
+    print("average accuracy: %f" % (np.mean(acc)))
+    print("average accuracy: %f" % (np.mean(acc)), file=res_f)
--- src/preprocess.py
+++ src/preprocess.py
@@ -5,15 +5,20 @@ all_data = []
 data_path = "../data/new_weibo_13638"
 labels = os.listdir(data_path)
 
+
+# Preprocess data with format of [data1, data2, ...].
+# Each data is organized as (label, [word1, word2, ...])
 for label in labels:
     label_path = os.path.join(data_path, label)
     for item in os.listdir(label_path):
         file_name = os.path.join(label_path, item)
         with open(file_name) as f:
             contents = f.read().split()
+            # ignore empty files
             if len(contents) != 0:
                 data_item = (label, contents)
                 all_data.append(data_item)
 
+# save data into a binary file, which is easier to read.
 with open("../data/all_data.pkl", "wb") as f:
     pickle.dump(all_data, f)
