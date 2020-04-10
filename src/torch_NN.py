c0f2b437b43e9976f8198308c3cbae1e17f45919
yuanyehome <2252706531@qq.com>
2020-04-10 23:42:13 +0800

    add file

--- src/torch_NN.py
+++ src/torch_NN.py
@@ -1,1 +1,10 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.embed = nn.Embedding
